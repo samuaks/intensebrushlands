5dee3352d850438f3d9ea048bad42dced54a4b0e
Samu <43408962+samuaks@users.noreply.github.com>
2019-05-13 08:35:17 +0300

    questions uncommented

--- src/main/java/open/vision/app/AppApplication.java
+++ src/main/java/open/vision/app/AppApplication.java
@@ -42,80 +42,80 @@ public class AppApplication {
 	public CommandLineRunner lukkariKysely(QuestionRepository qRepo, AnswerOptionRepository aRepo, AnswerRepository answerRepo) {
 		return (args) -> {
 			log.info("Please tell rosie");
-//									
-//			//Questions
-//			//1
-//			qRepo.save(new Question("Minä vuonna aloitit opintosi Haaga-Heliassa?", "monivalinta"));
-//			//2
-//			qRepo.save(new Question("Oletko käyttänyt Lukkarikonetta?", "monivalinta"));
-//			//3
-//			qRepo.save(new Question("Oletko kokenut Lukkarikoneen toimivaksi?", "monivalinta"));
-//			//4
-//			qRepo.save(new Question("Mitä hyvää lukkarikoneessa mielestäsi on?", "avoin"));
-//			//5
-//			qRepo.save(new Question("Onko sinulla parannusehdotuksia Lukkarikoneen toimintaan?", "avoin"));
-//			//6
-//			qRepo.save(new Question("Mikä näistä ominaisuuksista on hyödyllisin?", "monivalinta"));
-//			//7
-//			qRepo.save(new Question("Millä laitteella yleensä käytät Lukkarikonetta", "monivalinta"));
-//			//8
-//			qRepo.save(new Question("Käyttäisitkö Lukkarikonetta enemmän jos siitä olisi mobiiliversio tai sovellus?", "monivalinta"));
-//			//9
-//			qRepo.save(new Question("Kuinka usein tarkastelet lukujärjestystäsi Lukkarikoneesta?", "monivalinta"));
-//			//10
-//			qRepo.save(new Question("Haluaisitko itse vaikuttaa Lukkarikoneen ulkoasuun?", "monivalinta"));
-//			//11
-//		  //qRepo.save(new Question("Onko sinulla ollut ongelmia Lukkarikoneen käytössä?", "monivalinta"));
-//			
-//			//Answers to question 1
-//			aRepo.save(new AnswerOption("2019", qRepo.findByQuestionId(1).get(0)));
-//			aRepo.save(new AnswerOption("2018", qRepo.findByQuestionId(1).get(0)));
-//			aRepo.save(new AnswerOption("Aiemmin", qRepo.findByQuestionId(1).get(0)));
-//			
-//			//Answers to question 2
-//			aRepo.save(new AnswerOption("Kyllä", qRepo.findByQuestionId(2).get(0)));
-//			aRepo.save(new AnswerOption("Ei", qRepo.findByQuestionId(2).get(0)));
-//			
-//			//Answers to question 3
-//			aRepo.save(new AnswerOption("Kyllä", qRepo.findByQuestionId(3).get(0)));
-//			aRepo.save(new AnswerOption("Ei", qRepo.findByQuestionId(3).get(0)));
-//			aRepo.save(new AnswerOption("En osaa sanoa", qRepo.findByQuestionId(3).get(0)));
-//			
-//			//Answer to question 4
-//			aRepo.save(new AnswerOption("", qRepo.findByQuestionId(4).get(0)));
-//			
-//			//Answer to question 5
-//			aRepo.save(new AnswerOption("", qRepo.findByQuestionId(5).get(0)));
-//			
-//			//Answers to question 6
-//			aRepo.save(new AnswerOption("Lukujärjestyksen tallentaminen", qRepo.findByQuestionId(6).get(0)));
-//			aRepo.save(new AnswerOption("Lukujärjestyksen kasaaminen", qRepo.findByQuestionId(6).get(0)));
-//			aRepo.save(new AnswerOption("Kurssin hakeminen kurssin nimellä", qRepo.findByQuestionId(6).get(0)));
-//			aRepo.save(new AnswerOption("Kurssin hakeminen kurssitunnuksella", qRepo.findByQuestionId(6).get(0)));
-//			aRepo.save(new AnswerOption("Kurssien hakeminen oman lukujärjestyksen tyhjiin kohtiin", qRepo.findByQuestionId(6).get(0)));
-//			aRepo.save(new AnswerOption("Valmiiden lukujärjestysten etsiminen ryhmätunnuksella", qRepo.findByQuestionId(6).get(0)));
-//
-//			//Answers to question 7
-//			aRepo.save(new AnswerOption("Tietokoneella", qRepo.findByQuestionId(7).get(0)));
-//			aRepo.save(new AnswerOption("Puhelin / Tabletti", qRepo.findByQuestionId(7).get(0)));
-//			aRepo.save(new AnswerOption("Molemmilla", qRepo.findByQuestionId(7).get(0)));
-//			
-//			//Answers to question 8
-//			aRepo.save(new AnswerOption("Kyllä", qRepo.findByQuestionId(8).get(0)));
-//			aRepo.save(new AnswerOption("En", qRepo.findByQuestionId(8).get(0)));
-//			aRepo.save(new AnswerOption("En osaa sanoa", qRepo.findByQuestionId(8).get(0)));
-//			
-//			//Answers to question 9
-//			aRepo.save(new AnswerOption("Päivittäin", qRepo.findByQuestionId(9).get(0)));
-//			aRepo.save(new AnswerOption("Viikottain", qRepo.findByQuestionId(9).get(0)));
-//			aRepo.save(new AnswerOption("Kuukausittain", qRepo.findByQuestionId(9).get(0)));
-//			aRepo.save(new AnswerOption("Harvemmin", qRepo.findByQuestionId(9).get(0)));
-//			aRepo.save(new AnswerOption("En koskaan", qRepo.findByQuestionId(9).get(0)));
-//
-//			//Answers to question 10
-//			aRepo.save(new AnswerOption("Kyllä", qRepo.findByQuestionId(10).get(0)));
-//			aRepo.save(new AnswerOption("En", qRepo.findByQuestionId(10).get(0)));
-//			
+									
+			//Questions
+			//1
+			qRepo.save(new Question("Minä vuonna aloitit opintosi Haaga-Heliassa?", "monivalinta"));
+			//2
+			qRepo.save(new Question("Oletko käyttänyt Lukkarikonetta?", "monivalinta"));
+			//3
+			qRepo.save(new Question("Oletko kokenut Lukkarikoneen toimivaksi?", "monivalinta"));
+			//4
+			qRepo.save(new Question("Mitä hyvää lukkarikoneessa mielestäsi on?", "avoin"));
+			//5
+			qRepo.save(new Question("Onko sinulla parannusehdotuksia Lukkarikoneen toimintaan?", "avoin"));
+			//6
+			qRepo.save(new Question("Mikä näistä ominaisuuksista on hyödyllisin?", "monivalinta"));
+			//7
+			qRepo.save(new Question("Millä laitteella yleensä käytät Lukkarikonetta", "monivalinta"));
+			//8
+			qRepo.save(new Question("Käyttäisitkö Lukkarikonetta enemmän jos siitä olisi mobiiliversio tai sovellus?", "monivalinta"));
+			//9
+			qRepo.save(new Question("Kuinka usein tarkastelet lukujärjestystäsi Lukkarikoneesta?", "monivalinta"));
+			//10
+			qRepo.save(new Question("Haluaisitko itse vaikuttaa Lukkarikoneen ulkoasuun?", "monivalinta"));
+			//11
+		  //qRepo.save(new Question("Onko sinulla ollut ongelmia Lukkarikoneen käytössä?", "monivalinta"));
+			
+			//Answers to question 1
+			aRepo.save(new AnswerOption("2019", qRepo.findByQuestionId(1).get(0)));
+			aRepo.save(new AnswerOption("2018", qRepo.findByQuestionId(1).get(0)));
+			aRepo.save(new AnswerOption("Aiemmin", qRepo.findByQuestionId(1).get(0)));
+			
+			//Answers to question 2
+			aRepo.save(new AnswerOption("Kyllä", qRepo.findByQuestionId(2).get(0)));
+			aRepo.save(new AnswerOption("Ei", qRepo.findByQuestionId(2).get(0)));
+			
+			//Answers to question 3
+			aRepo.save(new AnswerOption("Kyllä", qRepo.findByQuestionId(3).get(0)));
+			aRepo.save(new AnswerOption("Ei", qRepo.findByQuestionId(3).get(0)));
+			aRepo.save(new AnswerOption("En osaa sanoa", qRepo.findByQuestionId(3).get(0)));
+			
+			//Answer to question 4
+			aRepo.save(new AnswerOption("", qRepo.findByQuestionId(4).get(0)));
+			
+			//Answer to question 5
+			aRepo.save(new AnswerOption("", qRepo.findByQuestionId(5).get(0)));
+			
+			//Answers to question 6
+			aRepo.save(new AnswerOption("Lukujärjestyksen tallentaminen", qRepo.findByQuestionId(6).get(0)));
+			aRepo.save(new AnswerOption("Lukujärjestyksen kasaaminen", qRepo.findByQuestionId(6).get(0)));
+			aRepo.save(new AnswerOption("Kurssin hakeminen kurssin nimellä", qRepo.findByQuestionId(6).get(0)));
+			aRepo.save(new AnswerOption("Kurssin hakeminen kurssitunnuksella", qRepo.findByQuestionId(6).get(0)));
+			aRepo.save(new AnswerOption("Kurssien hakeminen oman lukujärjestyksen tyhjiin kohtiin", qRepo.findByQuestionId(6).get(0)));
+			aRepo.save(new AnswerOption("Valmiiden lukujärjestysten etsiminen ryhmätunnuksella", qRepo.findByQuestionId(6).get(0)));
+
+			//Answers to question 7
+			aRepo.save(new AnswerOption("Tietokoneella", qRepo.findByQuestionId(7).get(0)));
+			aRepo.save(new AnswerOption("Puhelin / Tabletti", qRepo.findByQuestionId(7).get(0)));
+			aRepo.save(new AnswerOption("Molemmilla", qRepo.findByQuestionId(7).get(0)));
+			
+			//Answers to question 8
+			aRepo.save(new AnswerOption("Kyllä", qRepo.findByQuestionId(8).get(0)));
+			aRepo.save(new AnswerOption("En", qRepo.findByQuestionId(8).get(0)));
+			aRepo.save(new AnswerOption("En osaa sanoa", qRepo.findByQuestionId(8).get(0)));
+			
+			//Answers to question 9
+			aRepo.save(new AnswerOption("Päivittäin", qRepo.findByQuestionId(9).get(0)));
+			aRepo.save(new AnswerOption("Viikottain", qRepo.findByQuestionId(9).get(0)));
+			aRepo.save(new AnswerOption("Kuukausittain", qRepo.findByQuestionId(9).get(0)));
+			aRepo.save(new AnswerOption("Harvemmin", qRepo.findByQuestionId(9).get(0)));
+			aRepo.save(new AnswerOption("En koskaan", qRepo.findByQuestionId(9).get(0)));
+
+			//Answers to question 10
+			aRepo.save(new AnswerOption("Kyllä", qRepo.findByQuestionId(10).get(0)));
+			aRepo.save(new AnswerOption("En", qRepo.findByQuestionId(10).get(0)));
+			
 			//Answers to question 11
 //			aRepo.save(new AnswerOption("Ei", qRepo.findByQuestionId(11).get(0)));
 //			aRepo.save(new AnswerOption("Kyllä, mitä", qRepo.findByQuestionId(11).get(0)));
