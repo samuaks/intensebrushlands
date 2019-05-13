14aa709d092fd0e79491f21273ede90ae2acb1b7
Samu <43408962+samuaks@users.noreply.github.com>
2019-05-13 10:24:48 +0300

    Delete UserRepository.java

--- src/main/java/open/vision/app/domain/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package open.vision.app.domain;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User, Long> {
-		
-	User findByUsername(String username);
-}
